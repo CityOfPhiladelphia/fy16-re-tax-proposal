ae063b6dc95a25defaa8b89734beb3756489e911
Aaron Ogle <atogle@gmail.com>
2015-05-05 17:33:10 -0400

    Add less absurd test data.

--- js/data.js
+++ js/data.js
@@ -4,48 +4,48 @@ app.data = app.data || {};
 
 app.data.schools = {
   test: {
-    'principals_current': 123,
-    'principals_proposed': 123,
-    'principals_diff': 123,
+    'principals_current': 10,
+    'principals_proposed': 15,
+    'principals_diff': 5,
 
-    'teachers_current': 123,
-    'teachers_proposed': 123,
-    'teachers_diff': 123,
+    'teachers_current': 10,
+    'teachers_proposed': 15,
+    'teachers_diff': 5,
 
-    'special_ed_current': 123,
-    'special_ed_proposed': 123,
-    'special_ed_diff': 123,
+    'special_ed_current': 10,
+    'special_ed_proposed': 15,
+    'special_ed_diff': 5,
 
-    'counselors_current': 123,
-    'counselors_proposed': 123,
-    'counselors_diff': 123,
+    'counselors_current': 10,
+    'counselors_proposed': 15,
+    'counselors_diff': 5,
 
-    'nurses_current': 123,
-    'nurses_proposed': 123,
-    'nurses_diff': 123,
+    'nurses_current': 10,
+    'nurses_proposed': 15,
+    'nurses_diff': 5,
 
-    'assistants_current': 123,
-    'assistants_proposed': 123,
-    'assistants_diff': 123,
+    'assistants_current': 10,
+    'assistants_proposed': 15,
+    'assistants_diff': 5,
 
-    'secretaries_current': 123,
-    'secretaries_proposed': 123,
-    'secretaries_diff': 123,
+    'secretaries_current': 10,
+    'secretaries_proposed': 15,
+    'secretaries_diff': 5,
 
-    'support_current': 123,
-    'support_proposed': 123,
-    'support_diff': 123,
+    'support_current': 10,
+    'support_proposed': 15,
+    'support_diff': 5,
 
-    'noon_aides_current': 123,
-    'noon_aides_proposed': 123,
-    'noon_aides_diff': 123,
+    'noon_aides_current': 10,
+    'noon_aides_proposed': 15,
+    'noon_aides_diff': 5,
 
-    'other_current': 123,
-    'other_proposed': 123,
-    'other_diff': 123,
+    'other_current': 10,
+    'other_proposed': 15,
+    'other_diff': 5,
 
-    'supplies_current': 123,
-    'supplies_proposed': 123,
-    'supplies_diff': 123
+    'supplies_current': 10000,
+    'supplies_proposed': 15000,
+    'supplies_diff': 5000
   }
 };
